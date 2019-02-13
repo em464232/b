2e8154a84c320d3bc6b7e18679669913bcac3cc8
em464232 <47606937+em464232@users.noreply.github.com>
2019-02-13 19:33:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-"
+var prefix = "A"
 
 
 client.on('ready', function() {
@@ -90,7 +90,7 @@ msg.delete();
 //////////////////////////////
 client.on("ready", () => { // كود رينبو
   function lol() {
-    client.guilds.get('511582404113072138').roles.find("name", "Rainbow").setColor("RANDOM");
+    client.guilds.get('531202861518815245').roles.find("name", "Rainbow").setColor("RANDOM");
   };
   setInterval(lol, 2000);
 });
